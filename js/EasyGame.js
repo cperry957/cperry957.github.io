bf99d1916e80aa820ecb528160a1af7d6ef183e3
cperry957 <49246253+cperry957@users.noreply.github.com>
2021-03-04 01:21:02 -0800

    Update EasyGame.js

--- js/EasyGame.js
+++ js/EasyGame.js
@@ -76,7 +76,7 @@ class EasyGame extends Phaser.Scene{
                 this.load.audio('backgroundMusic', 'assets/sounds/backgroundMusic.mp3');
 
 				this.load.image('bullet', 'assets/bullet.png');
-				this.load.image('ArrowClick', 'assets/Arrowclick.png);
+				this.load.image('ArrowClick', 'assets/Arrowclick.png');
 				this.load.image('ArrowTower', 'assets/ArrowTower.png');
 				this.load.image('ArrowTowerUpgrade', 'assets/ArrowTowerUpgrade.png');
 				
